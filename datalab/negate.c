f9a8079535f9ab7debba41a05e5103fc4c634c19
QASAK <w663012911@outlook.com>
2020-05-11 16:02:30 +0800

    Create negate.c

---
+++ datalab/negate.c
@@ -0,0 +1,15 @@
+/*
+ * negate - return -x
+ *   Example: negate(1) = -1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+ 
+/*
+    负
+ */
+ 
+int negate(int x) {
+  return (~x+1);
+}
