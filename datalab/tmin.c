76e4d56e1d1ed77e37fdd03b2ac0a0d5a3e864df
QASAK <w663012911@outlook.com>
2020-05-11 11:52:36 +0800

    Create tmin.c

---
+++ datalab/tmin.c
@@ -0,0 +1,10 @@
+/*
+ * tmin - return minimum two's complement integer
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 4
+ *   Rating: 1
+ */
+ int tmin(void) {
+    int ans=1<<31;
+    return ans;
+}
