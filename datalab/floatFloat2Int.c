2bdae2f02d41db3b0dfde13ace1d8c48ba86e90e
QASAK <w663012911@outlook.com>
2020-05-13 10:29:14 +0800

    Create floatFloat2Int.c

---
+++ datalab/floatFloat2Int.c
@@ -0,0 +1,14 @@
+/*
+ * floatFloat2Int - Return bit-level equivalent of expression (int) f
+ *   for floating point argument f.
+ *   Argument is passed as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point value.
+ *   Anything out of range (including NaN and infinity) should return
+ *   0x80000000u.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+ 
+ 
