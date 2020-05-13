72e420f6fd59c4094ad9821eb028b31ac0e80587
QASAK <w663012911@outlook.com>
2020-05-13 17:46:43 +0800

    Create floatPower2.c

---
+++ datalab/floatPower2.c
@@ -0,0 +1,20 @@
+/*
+ * floatPower2 - Return bit-level equivalent of the expression 2.0^x
+ *   (2.0 raised to the power x) for any 32-bit integer x.
+ *
+ *   The unsigned value that is returned should have the identical bit
+ *   representation as the single-precision floating-point number 2.0^x.
+ *   If the result is too small to be represented as a denorm, return
+ *   0. If too large, return +INF.
+ *
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. Also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+
+/*
+    返回2.0的x次幂(x是32位整数)
+    如果结果太小而不能表示为denorm,返回0
+    如果太大,返回+inf
+
+ */
