da0e509692d9a0eb167943cf5056b435e2371a0b
QASAK <w663012911@outlook.com>
2020-07-14 17:04:51 +0800

    Update dcl.c

--- bomblab/dcl.c
+++ bomblab/dcl.c
@@ -60,6 +60,36 @@ void dirdcl(void)
 	   }
 }
 
+
+int gettoken(void)  /* return next token */
+{
+   int c, getch(void);
+   void ungetch(int);
+   char *p = token;
+   while ((c = getch()) == ' ' || c == '\t')
+	   ;       if (c == '(') {
+	   if ((c = getch()) == ')') {
+		   strcpy(token, "()");
+		   return tokentype = PARENS;
+	   } else {
+		   ungetch(c);
+		   return tokentype = '(';
+	   }
+   } else if (c == '[') {
+	   for (*p++ = c; (*p++ = getch()) != ']'; )
+		   ;
+	   *p = '\0';
+	   return tokentype = BRACKETS;
+   } else if (isalpha(c)) {
+	   for (*p++ = c; isalnum(c = getch()); )
+		   *p++ = c;
+	   *p = '\0';
+	   ungetch(c);
+	   return tokentype = NAME;
+   } else
+	   return tokentype = c;
+}
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -87,6 +117,25 @@ main()  /* convert declaration to words */
 }
 
 
-
-
+/* undcl:  convert word descriptions to declarations */
+/*
+main()
+{
+   int type;
+   char temp[MAXTOKEN];
+   while (gettoken() != EOF) {
+	   strcpy(out, token);
+	   while ((type = gettoken()) != '\n')
+		   if (type == PARENS || type == BRACKETS)
+			   strcat(out, token);
+		   else if (type == '*') {
+			   sprintf(temp, "(*%s)", out);
+			   strcpy(out, temp);
+		   } else if (type == NAME) {
+			   sprintf(temp, "%s %s", token, out);
+			   strcpy(out, temp);
+		   } else                   printf("invalid input at %s\n", token);
+   }
+   return 0;
+} */
 
