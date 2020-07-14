9747b3f510c7cbd03343b63a21b4464db12eb3b5
QASAK <w663012911@outlook.com>
2020-07-14 17:07:46 +0800

    Update dcl.c

--- bomblab/dcl.c
+++ bomblab/dcl.c
@@ -29,6 +29,49 @@ direct-dcl name
 */
 
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAXTOKEN  100
+enum { NAME, PARENS, BRACKETS };
+void dcl(void);
+void dirdcl(void);
+int gettoken(void);
+int tokentype;           /* type of last token */
+char token[MAXTOKEN];    /* last token string */
+char name[MAXTOKEN];     /* identifier name */
+char datatype[MAXTOKEN]; /* data type = char, int, etc. */
+char out[1000];
+
+int gettoken(void)  /* return next token */
+{
+   int c, getch(void);
+   void ungetch(int);
+   char *p = token;
+   while ((c = getch()) == ' ' || c == '\t')
+	   ;       if (c == '(') {
+	   if ((c = getch()) == ')') {
+		   strcpy(token, "()");
+		   return tokentype = PARENS;
+	   } else {
+		   ungetch(c);
+		   return tokentype = '(';
+	   }
+   } else if (c == '[') {
+	   for (*p++ = c; (*p++ = getch()) != ']'; )
+		   ;
+	   *p = '\0';
+	   return tokentype = BRACKETS;
+   } else if (isalpha(c)) {
+	   for (*p++ = c; isalnum(c = getch()); )
+		   *p++ = c;
+	   *p = '\0';
+	   ungetch(c);
+	   return tokentype = NAME;
+   } else
+	   return tokentype = c;
+}
+
 /* dcl:  parse a declarator */
 void dcl(void)
 {
@@ -61,48 +104,8 @@ void dirdcl(void)
 }
 
 
-int gettoken(void)  /* return next token */
-{
-   int c, getch(void);
-   void ungetch(int);
-   char *p = token;
-   while ((c = getch()) == ' ' || c == '\t')
-	   ;       if (c == '(') {
-	   if ((c = getch()) == ')') {
-		   strcpy(token, "()");
-		   return tokentype = PARENS;
-	   } else {
-		   ungetch(c);
-		   return tokentype = '(';
-	   }
-   } else if (c == '[') {
-	   for (*p++ = c; (*p++ = getch()) != ']'; )
-		   ;
-	   *p = '\0';
-	   return tokentype = BRACKETS;
-   } else if (isalpha(c)) {
-	   for (*p++ = c; isalnum(c = getch()); )
-		   *p++ = c;
-	   *p = '\0';
-	   ungetch(c);
-	   return tokentype = NAME;
-   } else
-	   return tokentype = c;
-}
 
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#define MAXTOKEN  100
-enum { NAME, PARENS, BRACKETS };
-void dcl(void);
-void dirdcl(void);
-int gettoken(void);
-int tokentype;           /* type of last token */
-char token[MAXTOKEN];    /* last token string */
-char name[MAXTOKEN];     /* identifier name */
-char datatype[MAXTOKEN]; /* data type = char, int, etc. */
-char out[1000];
+
 main()  /* convert declaration to words */
 {
    while (gettoken() != EOF) {   /* 1st token on line */
