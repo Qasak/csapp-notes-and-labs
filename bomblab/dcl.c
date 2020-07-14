a75c8e82e884a74e9cb7372640f9c9d24fe6b620
QASAK <w663012911@outlook.com>
2020-07-14 17:15:21 +0800

    Update dcl.c

--- bomblab/dcl.c
+++ bomblab/dcl.c
@@ -33,6 +33,7 @@ direct-dcl name
 #include <string.h>
 #include <ctype.h>
 #define MAXTOKEN  100
+#define BUFSIZE 100
 enum { NAME, PARENS, BRACKETS };
 void dcl(void);
 void dirdcl(void);
@@ -42,10 +43,24 @@ char token[MAXTOKEN];    /* last token string */
 char name[MAXTOKEN];     /* identifier name */
 char datatype[MAXTOKEN]; /* data type = char, int, etc. */
 char out[1000];
+char buf[BUFSIZE];    /* buffer for ungetch */
+int bufp = 0;         /* next free position in buf */
+int getch(void)  /* get a (possibly pushed-back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)   /* push character back on input */
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
 
 int gettoken(void)  /* return next token */
 {
-   int c, getch(void);
+   int c;
    void ungetch(int);
    char *p = token;
    while ((c = getch()) == ' ' || c == '\t')
@@ -106,7 +121,7 @@ void dirdcl(void)
 
 
 
-main()  /* convert declaration to words */
+int main()  /* convert declaration to words */
 {
    while (gettoken() != EOF) {   /* 1st token on line */
 	   strcpy(datatype, token);  /* is the datatype */
